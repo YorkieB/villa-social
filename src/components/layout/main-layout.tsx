bdfd97f0a3113037d835a2211cbd2fbb5484eb5d
Yorkie Brown <yorkie@yorkiebrown.uk>
2026-06-07 20:10:48 +0100

    Update main-layout.tsx

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -3,6 +3,9 @@ import { Toaster } from 'react-hot-toast';
 import { fetchJSON } from '@lib/fetch';
 import { WindowContextProvider } from '@lib/context/window-context';
 import { Sidebar } from '@components/sidebar/sidebar';
+import { Aside } from '@components/aside/aside';
+import { AsideTrends } from '@components/aside/aside-trends';
+import { Suggestions } from '@components/aside/suggestions';
 import type { DefaultToastOptions } from 'react-hot-toast';
 import type { LayoutProps } from './common-layout';
 
@@ -17,11 +20,27 @@ const toastOptions: DefaultToastOptions = {
 
 export function MainLayout({ children }: LayoutProps): JSX.Element {
   return (
-    <div className='flex w-full justify-center gap-0 lg:gap-4'>
+    <div className='flex w-full justify-center gap-0 lg:gap-4 bg-main-background min-h-screen'>
       <WindowContextProvider>
+        {/* Column 1: Left Navigation Sidebar */}
         <Sidebar />
-        <SWRConfig value={{ fetcher: fetchJSON }}>{children}</SWRConfig>
+        
+        {/* Column 2: Central Feed Column */}
+        <main className='flex-1 max-w-2xl border-x border-light-border/40 dark:border-dark-border/40 min-h-screen'>
+          <SWRConfig value={{ fetcher: fetchJSON }}>
+            {children}
+          </SWRConfig>
+        </main>
+
+        {/* Column 3: Right Sidebar with Modern Floating Frosted Cards */}
+        <Aside>
+          <div className='flex flex-col gap-4 bg-transparent'>
+            <AsideTrends />
+            <Suggestions />
+          </div>
+        </Aside>
       </WindowContextProvider>
+
       <Toaster
         position='bottom-center'
         toastOptions={toastOptions}
