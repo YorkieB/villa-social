689876c0379819a63d73b06fcd3c890428000e9d
Yorkie Brown <yorkie@yorkiebrown.uk>
2026-06-07 19:12:54 +0100

    Update aside-trends.tsx

--- src/components/aside/aside-trends.tsx
+++ src/components/aside/aside-trends.tsx
@@ -9,12 +9,11 @@ import { HeroIcon } from '@components/ui/hero-icon';
 import { Button } from '@components/ui/button';
 import { ToolTip } from '@components/ui/tooltip';
 import { Loading } from '@components/ui/loading';
-import type { MotionProps } from 'framer-motion';
+import type { Variants } from 'framer-motion';
 
-export const variants: MotionProps = {
+export const variants: Variants = {
   initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  transition: { duration: 0.8 }
+  animate: { opacity: 1, transition: { duration: 0.8 } }
 };
 
 type AsideTrendsProps = {
@@ -43,51 +42,52 @@ export function AsideTrends({ inTrendsPage }: AsideTrendsProps): JSX.Element {
           {...variants}
         >
           {!inTrendsPage && (
-            <h2 className='text-xl font-extrabold'>Trends for you</h2>
+            <h2 className="text-xl font-extrabold">Trends for you</h2>
           )}
           {trends.map(({ name, query, tweet_volume, url }) => (
-            <Link href={url} key={query}>
-              <a
-                className='hover-animation accent-tab hover-card relative 
-                           flex cursor-not-allowed flex-col gap-0.5'
-                onClick={preventBubbling()}
-              >
-                <div className='absolute right-2 top-2'>
-                  <Button
-                    className='hover-animation group relative cursor-not-allowed p-2
-                               hover:bg-main-accent/10 focus-visible:bg-main-accent/20 
-                               focus-visible:!ring-main-accent/80'
-                    onClick={preventBubbling()}
-                  >
-                    <HeroIcon
-                      className='h-5 w-5 text-light-secondary group-hover:text-main-accent 
-                                 group-focus-visible:text-main-accent dark:text-dark-secondary'
-                      iconName='EllipsisHorizontalIcon'
-                    />
-                    <ToolTip tip='More' />
-                  </Button>
-                </div>
-                <p className='text-sm text-light-secondary dark:text-dark-secondary'>
-                  Trending{' '}
-                  {location === 'Worldwide'
-                    ? 'Worldwide'
-                    : `in ${location as string}`}
-                </p>
-                <p className='font-bold'>{name}</p>
-                <p className='text-sm text-light-secondary dark:text-dark-secondary'>
+            <Link
+              href={url}
+              key={query}
+              className="hover-animation accent-tab hover-card relative
+                         flex flex-col gap-0.5"
+              onClick={(e) => e.preventDefault()}
+            >
+              <div className="absolute right-2 top-2">
+                <Button
+                  className="hover-animation group relative p-2
+                             hover:bg-main-accent/10 focus-visible:bg-main-accent/20
+                             focus-visible:!ring-main-accent/80"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <HeroIcon
+                    className="h-5 w-5 text-light-secondary group-hover:text-main-accent
+                               group-focus-visible:text-main-accent dark:text-dark-secondary"
+                    iconName="EllipsisHorizontalIcon"
+                  />
+                  <ToolTip tip="More" />
+                </Button>
+              </div>
+              <p className="text-sm text-light-secondary dark:text-dark-secondary">
+                Trending{' '}
+                {location === 'Worldwide' || !location
+                  ? 'Worldwide'
+                  : `in ${location}`}
+              </p>
+              <p className="font-bold">{name}</p>
+              {tweet_volume && (
+                <p className="text-sm text-light-secondary dark:text-dark-secondary">
                   {formatNumber(tweet_volume)} tweets
                 </p>
-              </a>
+              )}
             </Link>
           ))}
           {!inTrendsPage && (
-            <Link href='/trends'>
-              <a
-                className='custom-button accent-tab hover-card block w-full rounded-2xl
-                           rounded-t-none text-center text-main-accent'
-              >
-                Show more
-              </a>
+            <Link
+              href="/trends"
+              className="custom-button accent-tab hover-card block w-full rounded-2xl
+                         rounded-t-none text-center text-main-accent"
+            >
+              Show more
             </Link>
           )}
         </motion.div>
