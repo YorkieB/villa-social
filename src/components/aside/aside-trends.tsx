8221cfd4fdb22a065c6e9e40a2e84f8cb7f9af3f
Yorkie Brown <yorkie@yorkiebrown.uk>
2026-06-07 18:38:56 +0100

    Update aside-trends.tsx

--- src/components/aside/aside-trends.tsx
+++ src/components/aside/aside-trends.tsx
@@ -1,20 +1,20 @@
-import Link from "next/link";
-import cn from "clsx";
-import { motion } from "framer-motion";
-import { formatNumber } from "@lib/date";
-import { preventBubbling } from "@lib/utils";
-import { useTrends } from "@lib/api/trends";
-import { Error } from "@components/ui/error";
-import { HeroIcon } from "@components/ui/hero-icon";
-import { Button } from "@components/ui/button";
-import { ToolTip } from "@components/ui/tooltip";
-import { Loading } from "@components/ui/loading";
-import type { MotionProps } from "framer-motion";
+import Link from 'next/link';
+import cn from 'clsx';
+import { motion } from 'framer-motion';
+import { formatNumber } from '@lib/date';
+import { preventBubbling } from '@lib/utils';
+import { useTrends } from '@lib/api/trends';
+import { Error } from '@components/ui/error';
+import { HeroIcon } from '@components/ui/hero-icon';
+import { Button } from '@components/ui/button';
+import { ToolTip } from '@components/ui/tooltip';
+import { Loading } from '@components/ui/loading';
+import type { MotionProps } from 'framer-motion';
 
 export const variants: MotionProps = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
-  transition: { duration: 0.8 },
+  transition: { duration: 0.8 }
 };
 
 type AsideTrendsProps = {
@@ -23,7 +23,7 @@ type AsideTrendsProps = {
 
 export function AsideTrends({ inTrendsPage }: AsideTrendsProps): JSX.Element {
   const { data, loading } = useTrends(1, inTrendsPage ? 100 : 10, {
-    refreshInterval: 30000,
+    refreshInterval: 30000
   });
 
   const { trends, location } = data ?? {};
@@ -32,59 +32,59 @@ export function AsideTrends({ inTrendsPage }: AsideTrendsProps): JSX.Element {
     <section
       className={cn(
         !inTrendsPage &&
-          "hover-animation rounded-3xl bg-main-sidebar-background/60 backdrop-blur-md border border-light-border/40 dark:border-dark-border/40 shadow-card",
+          'hover-animation rounded-3xl bg-main-sidebar-background/60 backdrop-blur-md border border-light-border/40 dark:border-dark-border/40 shadow-card'
       )}
     >
       {loading ? (
         <Loading />
       ) : trends ? (
         <motion.div
-          className={cn("inner:px-4 inner:py-3", inTrendsPage && "mt-0.5")}
+          className={cn('inner:px-4 inner:py-3', inTrendsPage && 'mt-0.5')}
           {...variants}
         >
           {!inTrendsPage && (
-            <h2 className="text-xl font-extrabold">Trends for you</h2>
+            <h2 className='text-xl font-extrabold'>Trends for you</h2>
           )}
           {trends.map(({ name, query, tweet_volume, url }) => (
             <Link href={url} key={query}>
               <a
-                className="hover-animation accent-tab hover-card relative 
-                           flex cursor-not-allowed flex-col gap-0.5"
+                className='hover-animation accent-tab hover-card relative 
+                           flex cursor-not-allowed flex-col gap-0.5'
                 onClick={preventBubbling()}
               >
-                <div className="absolute right-2 top-2">
+                <div className='absolute right-2 top-2'>
                   <Button
-                    className="hover-animation group relative cursor-not-allowed p-2
+                    className='hover-animation group relative cursor-not-allowed p-2
                                hover:bg-main-accent/10 focus-visible:bg-main-accent/20 
-                               focus-visible:!ring-main-accent/80"
+                               focus-visible:!ring-main-accent/80'
                     onClick={preventBubbling()}
                   >
                     <HeroIcon
-                      className="h-5 w-5 text-light-secondary group-hover:text-main-accent 
-                                 group-focus-visible:text-main-accent dark:text-dark-secondary"
-                      iconName="EllipsisHorizontalIcon"
+                      className='h-5 w-5 text-light-secondary group-hover:text-main-accent 
+                                 group-focus-visible:text-main-accent dark:text-dark-secondary'
+                      iconName='EllipsisHorizontalIcon'
                     />
-                    <ToolTip tip="More" />
+                    <ToolTip tip='More' />
                   </Button>
                 </div>
-                <p className="text-sm text-light-secondary dark:text-dark-secondary">
-                  Trending{" "}
-                  {location === "Worldwide"
-                    ? "Worldwide"
+                <p className='text-sm text-light-secondary dark:text-dark-secondary'>
+                  Trending{' '}
+                  {location === 'Worldwide'
+                    ? 'Worldwide'
                     : `in ${location as string}`}
                 </p>
-                <p className="font-bold">{name}</p>
-                <p className="text-sm text-light-secondary dark:text-dark-secondary">
+                <p className='font-bold'>{name}</p>
+                <p className='text-sm text-light-secondary dark:text-dark-secondary'>
                   {formatNumber(tweet_volume)} tweets
                 </p>
               </a>
             </Link>
           ))}
           {!inTrendsPage && (
-            <Link href="/trends">
+            <Link href='/trends'>
               <a
-                className="custom-button accent-tab hover-card block w-full rounded-2xl
-                           rounded-t-none text-center text-main-accent"
+                className='custom-button accent-tab hover-card block w-full rounded-2xl
+                           rounded-t-none text-center text-main-accent'
               >
                 Show more
               </a>
